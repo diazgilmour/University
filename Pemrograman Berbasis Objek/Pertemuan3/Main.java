95772b2874607149466aeb9534652fc0c1be89d9
diazgilmour <diazziad1@gmail.com>
2024-09-03 18:57:57 +0700

    Add files via upload

---
+++ Pemrograman Berbasis Objek/Pertemuan3/Main.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tugaspraktikum3;
+
+/**
+ *
+ * @author khrns
+ */
+public class Main {
+    public static void main(String[] args) { 
+        Mobil diaz = new Mobil("Honda", "Brio", 2024, "Coklat");
+        diaz.displayInfo();
+        diaz.startEngine();
+        
+        diaz.setWarna("Putih");
+        
+        System.out.println("Warna mobil setelah perubahan warna : ");
+        diaz.displayInfo();
+    }
+    
+}
