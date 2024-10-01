28a5db57edb1e4dea3004ce8b6df87f355bc6c74
diazgilmour <diazziad1@gmail.com>
2024-10-01 08:07:59 +0700

    Add files via upload

---
+++ Pemrograman Berbasis Objek/ResponsiUTS-Diaz/ResponsiUTSDiaz/src/main/java/responsi/Main.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package responsi;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Main {
+    public static void main(String[] args) {
+        Produk laptop = new Elektronik("Laptop", 15000000, 2);
+        Produk snack = new Makanan("Snack", 15000, "2023-12-30");
+        
+        Pegawai diaz = new PegawaiTetap("Diaz", 5000000, 1000000);
+        Pegawai andi = new PegawaiKontrak("Andi", 3000000, 12);
+        
+        System.out.println("Output Produk : ");
+        laptop.tampilkanInfo();
+        
+        System.out.println("\nOutput Pegawai : ");
+        diaz.tampilkanInfo();
+        
+        System.out.println("\nOutput Polimorfisme : ");
+        snack.tampilkanInfo();
+        System.out.println("                ");
+        andi.tampilkanInfo();
+        
+        
+        
+    }
+    
+}
