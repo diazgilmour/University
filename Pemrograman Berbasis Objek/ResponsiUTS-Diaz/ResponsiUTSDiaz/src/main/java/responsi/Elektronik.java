c24003fd9ae7f536197047ae49043a5923af0060
diazgilmour <diazziad1@gmail.com>
2024-10-01 08:03:21 +0700

    Add files via upload

---
+++ Pemrograman Berbasis Objek/ResponsiUTS-Diaz/ResponsiUTSDiaz/src/main/java/responsi/Elektronik.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package responsi;
+
+/**
+ *
+ * @author ASUS
+ */
+class Elektronik extends Produk {
+    private int garansi;
+    
+    public Elektronik(String namaProduk, int harga, int garansi) {
+        super(namaProduk, harga);
+        this.garansi = garansi;
+    }
+    
+    @Override
+    public void tampilkanInfo() {
+        super.tampilkanInfo();
+        System.out.println("Garansi :" + garansi + "tahun");
+    
+    }
+    
+    
+    
+
+    
+}
---
+++ Pemrograman Berbasis Objek/ResponsiUTS-Diaz/ResponsiUTSDiaz/src/main/java/responsi/PegawaiKontrak.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package responsi;
+
+/**
+ *
+ * @author ASUS
+ */
+public class PegawaiKontrak extends Pegawai {
+    private int lamaKontrak;
+    
+    public PegawaiKontrak(String namaPegawai, int gaji, int lamaKontrak) {
+        super(namaPegawai, gaji);
+        this.lamaKontrak = lamaKontrak;
+    }
+    
+    @Override
+    public void tampilkanInfo() {
+        super.tampilkanInfo();
+        System.out.println("Lama Kontrak : " + lamaKontrak + "Tahun");
+    }
+    
+}
