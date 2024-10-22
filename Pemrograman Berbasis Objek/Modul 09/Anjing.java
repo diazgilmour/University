7ebb5ccc3b95e912e49029b748c67a2b88603982
diazgilmour <diazziad1@gmail.com>
2024-10-22 08:21:54 +0700

    Add files via upload

---
+++ Pemrograman Berbasis Objek/Modul 09/Anjing.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package soal_praktikum9;
+
+/**
+ *
+ * @author ASUS
+ */
+class Anjing extends Hewan {
+    @Override
+    void suara() {
+        System.out.println("Guk Guk");
+    }
+    
+}
